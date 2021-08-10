585d511c1e31aae382a9daa4be94dc3e47c1a1e7
052hide <hidetoshi1990@gmail.com>
2021-08-10 14:51:34 +0900

    web componentを使えるようにする

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
+<svelte:options tag="my-app" />
+
 <script lang="ts">
-  import Button from "~/lib/Button/index.svelte";
+  import "~/lib/Button/index.svelte";
   import "~/assets/style.css";
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen">
   <h1 class="text-3xl font-bold">Vite Svelte TS</h1>
 
-  <Button theme="secondary" size="base">Button</Button>
+  <atom-button theme="secondary" size="base">Button</atom-button>
 </main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), svelte()]
+  plugins: [
+    tsconfigPaths(),
+    svelte({
+      preprocess: sveltePreprocess(),
+      compilerOptions: {
+        customElement: true
+      }
+    })
+  ],
 })

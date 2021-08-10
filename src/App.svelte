3d3d7decf8600cae249118ae2439a72f083c15ac
052hide <hidetoshi1990@gmail.com>
2021-08-10 10:18:48 +0900

    絶対パスを使えるようにする

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import Button from "./lib/Button/index.svelte";
-  import "./assets/style.css";
+  import Button from "~/lib/Button/index.svelte";
+  import "~/assets/style.css";
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen">
